dcc33d0b9d35b0f3f5c2c27180deb0f2e2a5c3fa
ayakix <ayakix1935@gmail.com>
2016-07-05 22:48:48 +0900

    Update jQuery minor version

--- src/main/webapp/js/citywalk-dev.js
+++ src/main/webapp/js/citywalk-dev.js
@@ -1,8 +1,7 @@
 document.write('<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" />');
 document.write('<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" />');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>');
+
+document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>');
 document.write('<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>');
-document.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>');
 document.write('<script src="js/jsonrpc.js"></script>');
 document.write('<script src="js/parseUri.js"></script>');
