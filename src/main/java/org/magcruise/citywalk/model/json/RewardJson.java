8e3d956a9fd2b18b67806d2e271bf7d71cc9cf46
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-08-05 11:11:11 +0900

    initial_data.jsonのfetchをjson-rpc経由にした．

--- src/main/java/org/magcruise/citywalk/model/json/RewardJson.java
+++ src/main/java/org/magcruise/citywalk/model/json/RewardJson.java
@@ -3,6 +3,9 @@ package org.magcruise.citywalk.model.json;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class RewardJson {
 
 	private List<String> badges = new ArrayList<>();
@@ -23,6 +26,11 @@ public class RewardJson {
 
 	public void setRank(int rank) {
 		this.rank = rank;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
 	}
 
 }
